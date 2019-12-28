a8cfd7a26babaa83495b3b63c100f9c3a591333b
seatacken <57050116+seatacken@users.noreply.github.com>
2019-12-27 17:46:37 -0800

    Created functioning bracket tables with winners and losers based on points yay

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 'use strict';
 
+function OrganicPlayer(name, points){
+  this.name = name;
+  this.points = points;
+}
+
 //get player names
 function player1Name(){
   var getPlayerName = prompt('Player 1, what is your name?');
@@ -11,19 +16,101 @@ function player2Name(){
   getPlayerName;
   return getPlayerName;
 }
+function player3Name(){
+  var getPlayerName = prompt('Player 3, what is your name?');
+  getPlayerName;
+  return getPlayerName;
+}
+function player4Name(){
+  var getPlayerName = prompt('Player 4, what is your name?');
+  getPlayerName;
+  return getPlayerName;
+}
 
 //player names in vars for later use
 var player1 = player1Name();
 var player2 = player2Name();
-//adding organic players to document
-function organicPlayers(){
-  var table = document.getElementById('players');
+var player3 = player3Name();
+var player4 = player4Name();
+//adding first set of organic players to document
+function organicPlayers1(){
+  var table = document.getElementById('players1');
   var newTD = document.createElement('td');
   var addText = document.createTextNode(` ${player1} vs ${player2} Get ready!`);
   newTD.appendChild(addText);
   table.appendChild(newTD);
 }
-organicPlayers();
+//adding second set of organic players to document
+function organicPlayers2(){
+  var table = document.getElementById('players2');
+  var newTD = document.createElement('td');
+  var addText = document.createTextNode(` ${player3} vs ${player4} Get ready!`);
+  newTD.appendChild(addText);
+  table.appendChild(newTD);
+}
+organicPlayers1();
+organicPlayers2();
+//Giving life to our OrganicPlayers from our constructor
+var one = new OrganicPlayer(player1, 40);
+var two = new OrganicPlayer(player2, 40);
+var three = new OrganicPlayer(player3, 40);
+var four = new OrganicPlayer(player4, 40);
+
+// TODO!!  write a function to control these points
+one.points --;
+
+three.points --;
+//Decide who wins first upper set based on points
+function playerWinMatch1A(){
+  if (one.points <= two.points){
+    return one;
+  } else{
+    return two;
+  }
+}
+playerWinMatch1A();
+//Decide who wins lower set based on points
+function playerWinMatch1B(){
+  if (three.points <= four.points){
+    return three;
+  } else{
+    return four;
+  }
+}
+playerWinMatch1B();
+
+//MATCH2
+//Adding winners from match 1 to match 2
+function organicplayersMatch2(){
+  var table = document.getElementById('playersWin1');
+  var newTD = document.createElement('td');
+  var addText = document.createTextNode(` ${playerWinMatch1A().name} vs ${playerWinMatch1B().name} Get ready!`);
+  newTD.appendChild(addText);
+  table.appendChild(newTD);
+}
+organicplayersMatch2();
+//Return winner
+function win(){
+  if (playerWinMatch1A().points <= playerWinMatch1B().points){
+    return playerWinMatch1A().name;
+  } else{
+    return playerWinMatch1B().name;
+  }
+}
+win();
+//Render winner to screen
+function winRender(){
+  var table = document.getElementById('win');
+  var newTD = document.createElement('td');
+  var addText = document.createTextNode(` ${win()} wins the tournament!!`);
+  newTD.appendChild(addText);
+  table.appendChild(newTD);
+}
+winRender();
+
+
+//---cpus for stretch goal------------------------------------------
+
 //cpu constructor
 function Player(name, points){
   this.name = name;
@@ -56,11 +143,6 @@ cpuPlayers();
 
 
 //assign points to players
-
-
-
-
-
 
 // //working on this
 // function cpuPlayersMatch2(){
